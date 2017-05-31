72ff003a18f7617ee49aeeb56eea2c1134e15cf7
Sierra Palmer <stpalmer@wpi.edu>
2017-05-31 14:10:46 -0400

    added khepera initialization

--- drive.c
+++ drive.c
@@ -20,6 +20,19 @@ int main(int argc, char *argv[]){
 	kb_set_debug_level(2);
 	printf("libKhepera Template Program\r\n");
 
+	//initiate libkhepera and robot access
+	if (kh4_init(argc ,argv)!=0){
+		printf("\nERROR: could not initiate the libkhepera!\n\n");
+		return -1;
+	}
+
+	//open robot socket and store teh handle in their respective pointers
+	dsPic = knet_open("Khepera4:dsPic",KNET_BUS_I2C,0,NULL);
+
+	if(dsPic == NULL){
+		printf("\nERROR: could not initiate communication with Kh4 dspic\n\n:");
+		return -2;
+	}
 	kh4_set_speed(left,right,dsPic);
 }
 
